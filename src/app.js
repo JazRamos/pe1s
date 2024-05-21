053c062a7532b4a8beb5e39cbbfc69391353e109
jazr4 <jazminaramos4@hotmail.com>
2024-05-21 11:57:14 -0300

    pe1c

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import routerCart from '../src/routes/cartRouter.js'
-import productsRouter from '../src/routes/productsRouter.js';
-import morgan from 'morgan';
+import productsRouter from '../routes/productsRouter.js';
 import { __dirname } from './path.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 
