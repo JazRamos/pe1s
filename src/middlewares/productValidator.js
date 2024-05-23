11eba65967ce92976042057d13c60bd09c406af4
jazr4 <jazminaramos4@hotmail.com>
2024-05-23 19:10:05 -0300

    pe1d

--- src/middlewares/productValidator.js
+++ src/middlewares/productValidator.js
@@ -4,8 +4,8 @@ export const productValidator = (req, res, next) => {
         req.body.description === undefined ||
         req.body.code === undefined ||
         req.body.price === undefined ||
-        req.body.stock === undefined ||
-        req.body.category === undefined
+        req.body.stock === undefined 
+       
     ) res.status(404).json({ msg: 'Invalid body' });
     else next()
 }
--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -18,6 +18,7 @@ router.get('/', async (req, res,next) => {
         }
         res.status(200).json(datos)
     } catch (error) {
+       console.log(error)
         next(error);
     }
 })
